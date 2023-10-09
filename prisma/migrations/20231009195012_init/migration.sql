250282f2067887d50a3b5a75a744f648b4a19c01
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-10-09 16:24:52 -0400

    feat: Create inital migration

---
+++ prisma/migrations/20231009195012_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Store" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Store_pkey" PRIMARY KEY ("id")
+);
