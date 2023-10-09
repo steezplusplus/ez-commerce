e0d383746b749f3a3b4ce6bbb440c912bc6f3f76
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-10-09 17:09:01 -0400

    feat: Create seed

---
+++ prisma/seed.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function main() {
+  console.info('Starting seed');
+  const store = prisma.store.create({
+    data: {
+      name: "Jesse's Store",
+    }
+  });
+  console.info('Finished seed');
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  });
