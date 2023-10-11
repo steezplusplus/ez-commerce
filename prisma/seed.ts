e4ecd5fda33cc3e7b40af9e5a6005ff61ca08942
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-10-11 13:44:11 -0400

    feat: Create product

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,6 +4,8 @@ const prisma = new PrismaClient();
 async function main() {
   console.info('🌱 Starting seed');
 
+  await prisma.product.deleteMany();
+  await prisma.category.deleteMany();
   await prisma.store.deleteMany();
 
   const store = await prisma.store.create({
@@ -12,11 +14,18 @@ async function main() {
     }
   });
 
-  await prisma.category.create({
+  const category = await prisma.category.create({
     data: {
       name: 'Shirts',
       storeId: store.id,
     }
+  });
+
+  await prisma.product.create({
+    data: {
+      name: 'White Tee',
+      categoryId: category.id,
+    },
   });
 
   console.info('🌲 Finished seed');
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,19 @@ import { prisma } from '../../lib/db';
 
 export default async function Home() {
   const stores = await prisma.store.findMany();
+
   const categories = await prisma.category.findMany({
     where: {
       storeId: stores[0].id
     },
   })
+
+  const products = await prisma.product.findMany({
+    where: {
+      categoryId: categories[0].id
+    },
+  });
+
   console.log(stores);
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
@@ -15,9 +23,14 @@ export default async function Home() {
           <p key={store.id}>{store.name}</p>
         );
       })}
-      {categories.map((store) => {
+      {categories.map((category) => {
         return (
-          <p key={store.id}>{store.name}</p>
+          <p key={category.id}>{category.name}</p>
+        );
+      })}
+      {products.map((product) => {
+        return (
+          <p key={product.id}>{product.name}</p>
         );
       })}
     </main>
