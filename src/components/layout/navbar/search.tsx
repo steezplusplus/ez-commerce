03470bc246b1addd62bacd4ac788722d73681015
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-10-14 16:44:17 -0400

    fix: Fix routing bug

--- src/components/layout/navbar/search.tsx
+++ src/components/layout/navbar/search.tsx
@@ -6,19 +6,18 @@ import { useRouter, useSearchParams } from 'next/navigation';
 import { createUrl } from "lib/utils";
 
 const searchId = 'search-id';
-const pathName = 'search';
+const pathName = '/search';
 
 export function Search() {
   const router = useRouter();
-  const currentSearchParams = useSearchParams();
-  const inputDefaultValue = currentSearchParams?.get('q') || '';
+  const readOnlySearchParams = useSearchParams();
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const formElement = e.target as HTMLFormElement;
     const inputElement = formElement.search as HTMLInputElement;
-    const newParams = new URLSearchParams(currentSearchParams.toString());
+    const newParams = new URLSearchParams(readOnlySearchParams.toString());
 
     if (inputElement.value) {
       newParams.set('q', inputElement.value);
@@ -33,11 +32,12 @@ export function Search() {
     <form onSubmit={onSubmit} className="relative">
       <label className="sr-only" htmlFor={searchId}>Search products</label>
       <input
+        key={readOnlySearchParams?.get('q')}
         type="text"
         name="search"
         id={searchId}
         autoComplete="off"
-        defaultValue={inputDefaultValue}
+        defaultValue={readOnlySearchParams?.get('q') || ''}
         className="w-full rounded-lg border bg-white pr-4 pl-8 py-2 text-sm text-black dark:border-neutral-800 dark:bg-transparent dark:text-white"
       />
       <SearchIcon size="16" color="gray" className="absolute top-3 left-2"/>
