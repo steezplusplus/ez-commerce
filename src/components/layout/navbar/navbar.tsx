3e60ffc31798bcce73ff3c775a676d05181b2525
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-10-15 19:42:56 -0400

    feat: Remove nav links

--- src/components/layout/navbar/navbar.tsx
+++ src/components/layout/navbar/navbar.tsx
@@ -6,35 +6,13 @@ import { Search } from './search';
 
 export async function Navbar() {
   const store = await prisma.store.findFirstOrThrow({});
-  const categories = await prisma.category.findMany({
-    where: {
-      storeId: store.id,
-    },
-    take: 2,
-  });
 
   return (
     <nav className="flex w-full items-center justify-between p-4 lg:p-6">
       <div className="flex w-full items-center md:w-1/3">
         <Link href="/" className="mr-6 hover:underline">
-          <h1 className="text-md font-medium md:hidden lg:block">Jb.</h1>
+          <h1 className="text-md font-medium md:hidden lg:block">{store.name}</h1>
         </Link>
-        <div>
-          <ul className="mr-6 flex w-full gap-x-6 text-sm font-light">
-            <li>
-              <Link href="/search" className="hover:underline">
-                All
-              </Link>
-            </li>
-            {categories.map((category) => (
-              <li key={category.id}>
-                <Link href={`/search/${category.slug}`} className="hover:underline">
-                  {category.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
       </div>
 
       <div className="hidden items-center justify-center md:flex md:w-1/3">
