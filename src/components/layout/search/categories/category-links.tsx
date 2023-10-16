85bc82438dc946bc1adc27f55b0049e6f5f369cb
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-10-16 16:52:57 -0400

    feat: Category change persists search params

--- src/components/layout/search/categories/category-links.tsx
+++ src/components/layout/search/categories/category-links.tsx
@@ -2,15 +2,16 @@
 
 import { Category } from '@prisma/client';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { usePathname, useSearchParams } from 'next/navigation';
 
 export function CategoryLink(category: Category) {
   const path = usePathname();
+  const params = useSearchParams();
   const href = `/search/${category.slug}`;
   const active = path.endsWith(category.slug);
 
   return (
-    <Link className={active ? 'underline' : ''} href={href}>
+    <Link className={active ? 'underline' : ''} href={`${href}?${params.toString()}`}>
       {category.name}
     </Link>
   );
@@ -18,11 +19,12 @@ export function CategoryLink(category: Category) {
 
 export function DefaultCategoryLink() {
   const path = usePathname();
-  const href = '/search';
+  const params = useSearchParams();
+  const href = `/search`;
   const active = path === href;
 
   return (
-    <Link href={href} className={active ? 'underline' : ''}>
+    <Link href={`${href}?${params.toString()}`} className={active ? 'underline' : ''}>
       All
     </Link>
   );
