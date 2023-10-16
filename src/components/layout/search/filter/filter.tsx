b3526175d37e243a9b5d47a9f838e0e9c7298190
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-10-15 20:45:21 -0400

    feat: dry filter

--- src/components/layout/search/filter/filter.tsx
+++ src/components/layout/search/filter/filter.tsx
@@ -1,9 +1,13 @@
-const sortBy = [
-  'Relevance',
-  'Trending',
-  'Latest Arrivals',
-  'Price: Low to high',
-  'Price: High to low',
+import Link from 'next/link';
+
+type SortBy = {
+  name: string;
+  slug: string;
+};
+
+const sortBy: SortBy[] = [
+  { name: 'Price: Low to high', slug: 'price-asc' },
+  { name: 'Price: High to low', slug: 'price-desc' },
 ];
 
 export async function Filter() {
@@ -11,20 +15,20 @@ export async function Filter() {
     <nav>
       <h3 className="text-xs text-neutral-500 dark:text-neutral-400">Sort by</h3>
       <ul>
-        {sortBy.map((sort, i) => {
-          return (
-            <li
-              key={`${sort}-${i}`}
-              className="mt-2 flex text-sm text-black hover:underline dark:text-white"
-            >
-              <label className="flex items-center text-sm font-light">
-                {sort}
-                <input type="radio" className="hidden" name="sort-by" />
-              </label>
-            </li>
-          );
+        {sortBy.map((sort) => {
+          return <FilterLink key={sort.slug} {...sort} />;
         })}
       </ul>
     </nav>
+  );
+}
+
+function FilterLink(sort: SortBy) {
+  return (
+    <li className="mt-2 flex text-black hover:underline dark:text-white">
+      <Link href={`/search?sort=${sort.slug}`} className="text-sm font-light">
+        {sort.name}
+      </Link>
+    </li>
   );
 }
