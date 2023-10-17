d61b610ee5bc070cc1b45a1ef1e3f4f2eb3d9f72
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-10-16 20:37:39 -0400

    style: Header mobile layout

--- src/components/layout/navbar/navbar.tsx
+++ src/components/layout/navbar/navbar.tsx
@@ -1,4 +1,4 @@
-import { ShoppingCart } from 'lucide-react';
+import { AlignJustify, ShoppingCart } from 'lucide-react';
 import Link from 'next/link';
 
 import { prisma } from 'lib/db';
@@ -9,20 +9,29 @@ export async function Navbar() {
 
   return (
     <nav className="flex w-full items-center justify-between p-4 lg:p-6">
-      <div className="flex w-full items-center md:w-1/3">
-        <Link href="/" className="mr-6 hover:underline">
-          <h1 className="text-md font-medium md:hidden lg:block">{store.name}</h1>
-        </Link>
+      <div className="block flex-none md:hidden">
+        <button className="mr-2 rounded-md border p-2">
+          <AlignJustify size="16" />
+          {/* TODO  mobile menu */}
+        </button>
       </div>
 
-      <div className="hidden items-center justify-center md:flex md:w-1/3">
-        <Search />
-      </div>
+      <div className="flex w-full items-center">
+        <div className="flex w-full md:w-1/3 ">
+          <Link href="/" className="mr-6 hover:underline">
+            <h1 className="text-md font-medium">{store.name}</h1>
+          </Link>
+        </div>
 
-      <div className="flex justify-end md:w-1/3">
-        <Link href="/checkout" className="ml-auto rounded-md border p-2">
-          <ShoppingCart size="16" />
-        </Link>
+        <div className="hidden justify-center md:flex md:w-1/3">
+          <Search />
+        </div>
+
+        <div className="flex justify-end md:w-1/3">
+          <Link href="/checkout" className="ml-auto rounded-md border p-2">
+            <ShoppingCart size="16" />
+          </Link>
+        </div>
       </div>
     </nav>
   );
