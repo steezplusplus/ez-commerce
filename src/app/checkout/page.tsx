4f889b65693bf39eb04c0cdbe4aa7c1bdeffc88e
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-10-21 20:19:09 -0400

    refactor: Remove unused css

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,10 +1,11 @@
 import { Footer } from 'components/layout/footer/footer';
 import { Suspense } from 'react';
 
+// TODO Get items from localstorage
 export default function CheckoutPage() {
   return (
     <>
-      <div className="mx-auto min-h-screen max-w-screen-2xl gap-8 px-4 pb-4">
+      <div className="mx-auto min-h-screen max-w-screen-2xl px-4">
         <h2 className="text-4xl font-semibold">Checkout</h2>
       </div>
       <Suspense>
