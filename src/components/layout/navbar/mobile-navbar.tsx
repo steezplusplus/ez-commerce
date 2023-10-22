74219f477f2fffb7be915b4f8c6c8389111baf18
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-10-21 23:30:27 -0400

    style: Same size icon buttons

--- src/components/layout/navbar/mobile-navbar.tsx
+++ src/components/layout/navbar/mobile-navbar.tsx
@@ -1,10 +1,11 @@
 import { AlignJustify } from 'lucide-react';
 
+// TODO dialog
 export function MobileNavbar() {
   return (
     <>
       <button className="mr-2 rounded-md border p-2">
-        <AlignJustify size="16" />
+        <AlignJustify size="18" />
       </button>
       <dialog>dialog</dialog>
     </>
