104bce07eba08a7abce0e28f8156dae2012b3adb
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-10-23 14:23:54 -0400

    feat: Cleanup cart modal

--- src/components/layout/navbar/cart.tsx
+++ src/components/layout/navbar/cart.tsx
@@ -3,7 +3,7 @@
 import Link from 'next/link';
 import { useRef } from 'react';
 
-import { ShoppingCart } from 'lucide-react';
+import { ShoppingCart, X } from 'lucide-react';
 
 export function Cart() {
   const modaDialogRef = useRef<HTMLDialogElement>(null);
@@ -34,21 +34,21 @@ export function Cart() {
         autoFocus
         className="rounded-lg border p-2"
       >
-        <div className="mb-4">
-          <h3 className="mb-2">Cart</h3>
-          <ul className="pl-2">
-            <li>
-              <Link href={`/`}>Item</Link>
-            </li>
-          </ul>
+        <div className="mb-2 flex justify-between">
+          <h3 className="text-lg">Cart</h3>
+          <button onClick={closeModalDialog} className="rounded-md border border-neutral-800 p-1">
+            <X size="16" />
+          </button>
         </div>
-        <button
-          onClick={closeModalDialog}
-          className="rounded-md border border-neutral-800 px-2 py-1"
+        <ul className="mb-2">
+          <li>
+            <Link href={`/`}>Item</Link>
+          </li>
+        </ul>
+        <Link
+          href="/checkout"
+          className="rounded-mb border px-2 py-1 text-blue-500 hover:text-blue-700"
         >
-          Close
-        </button>
-        <Link href="/checkout" className="text-blue-500 hover:text-blue-700 hover:underline">
           Check out
         </Link>
       </dialog>
