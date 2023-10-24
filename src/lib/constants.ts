73a73fc4c4c5a2eff5e3e48f34e8e51cf6d27815
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-10-24 18:46:42 -0400

    feat: Create api route for search page and filter consts

---
+++ src/lib/constants.ts
@@ -0,0 +1,25 @@
+export type SortFilterItem = {
+  title: string;
+  slug: string | null;
+  sortKey: 'RELEVANCE' | 'PRICE';
+  order: 'asc' | 'desc';
+};
+
+export const defaultSort: SortFilterItem = {
+  title: 'Relevance', // TODO my relevance sort is actually by date created
+  slug: null,
+  sortKey: 'RELEVANCE',
+  order: 'asc',
+};
+
+export const sorting: SortFilterItem[] = [
+  defaultSort,
+  { title: 'Price: Low to high', slug: 'price-asc', sortKey: 'PRICE', order: 'asc' },
+  { title: 'Price: High to low', slug: 'price-desc', sortKey: 'PRICE', order: 'desc' },
+];
+
+export const TAGS = {
+  collections: 'collections',
+  products: 'products',
+  cart: 'cart',
+};
