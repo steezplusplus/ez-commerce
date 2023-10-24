aa432bf4e0da198f883a406d7bccde1d55ead8cd
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-10-24 19:55:59 -0400

    feat: Finish filter list

--- src/components/layout/search/filter/filter-item.tsx
+++ src/components/layout/search/filter/filter-item.tsx
@@ -1,4 +1,10 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname, useSearchParams } from 'next/navigation';
+
 import { SortFilterItem } from 'lib/constants';
+import { createUrl } from 'lib/utils';
 
 type FilterItemProps = {
   item: SortFilterItem;
@@ -6,5 +12,23 @@ type FilterItemProps = {
 
 export function FilterItem(props: FilterItemProps) {
   const { item } = props;
-  return <li>{item.title}</li>;
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const active = searchParams.get('sort') === item.slug;
+  const q = searchParams.get('q');
+  const href = createUrl(
+    pathname,
+    new URLSearchParams({
+      ...(q && { q }),
+      ...(item.slug && item.slug.length && { sort: item.slug }),
+    })
+  );
+
+  return (
+    <li className="mt-2 flex text-xs">
+      <Link className="w-full hover:underline" href={href}>
+        {item.title} {active ? ' <' : ''}
+      </Link>
+    </li>
+  );
 }
