6781f28e198fb74e64d027c172008c0629f46a09
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-10-24 16:53:09 -0400

    feat: Checkout page grid

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,5 +1,7 @@
-import { Footer } from 'components/layout/footer/footer';
 import { Suspense } from 'react';
+
+import { CartGrid } from 'components/checkout/cart-grid';
+import { Footer } from 'components/layout/footer/footer';
 
 // TODO Get items from localstorage
 export default function CheckoutPage() {
@@ -7,6 +9,7 @@ export default function CheckoutPage() {
     <>
       <div className="mx-auto min-h-screen max-w-screen-2xl px-4">
         <h2 className="text-4xl font-semibold">Checkout</h2>
+        <CartGrid />
       </div>
       <Suspense>
         <Footer />
---
+++ src/components/checkout/cart-grid.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+import { Grid, GridItem } from 'components/grid/grid';
+import { Price } from 'components/price/price';
+import { useCart } from 'hooks/use-cart';
+import { X } from 'lucide-react';
+
+export function CartGrid() {
+  const [isMounted, setIsMounted] = useState<boolean>(false);
+  const cartItems = useCart((cart) => cart.items);
+  const cart = useCart();
+
+  const onRemove = (productId: string) => {
+    cart.removeItem(productId);
+  };
+
+  // Avoid hydration error from using localstorage from useCart()
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <Grid>
+      {cartItems.map((product) => {
+        return (
+          <GridItem key={product.id}>
+            <div className="flex items-center justify-between">
+              {product.name}
+              <button
+                onClick={() => onRemove(product.id)}
+                className="rounded-md border border-neutral-200 p-1 dark:border-neutral-800"
+              >
+                <X size="12" />
+              </button>
+            </div>
+            <Price amount={String(product.price)} />
+          </GridItem>
+        );
+      })}
+    </Grid>
+  );
+}
