ef7698820231031d4c1e66d47e6e190df1c878e2
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-10-25 19:43:05 -0400

    style: Checkout spacing

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -7,10 +7,10 @@ export default function CheckoutPage() {
   return (
     <>
       <div className="mx-auto min-h-screen max-w-screen-2xl px-4">
-        <h2 className="text-4xl font-semibold">Checkout</h2>
-        <div className="my-4 px-4">
+        <div className="mb-8 space-y-4 px-4">
+          <h2 className="text-4xl font-semibold">Checkout</h2>
           <CartGrid />
-          <button className="my-4 w-full rounded-sm border p-2">Purchase</button>
+          <button className="w-full rounded-sm border p-2">Purchase</button>
         </div>
       </div>
       <Suspense>
