a5c1dd31a8582f59b40c8bceb290d7ce8fe6f315
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-10-29 18:30:14 -0400

    feat: product form controls search params

--- src/components/product/product-form.tsx
+++ src/components/product/product-form.tsx
@@ -1,3 +1,8 @@
+'use client';
+
+import { createUrl } from 'lib/utils';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+
 type ProductFormProps = {
   sizes: string[];
   colors: string[];
@@ -6,16 +11,35 @@ type ProductFormProps = {
 // TODO Form should control search params. Include them in product added to localstorage
 export function ProductForm(props: ProductFormProps) {
   const { sizes, colors } = props;
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const updateParams = (url: string): void => {
+    router.replace(url, { scroll: false });
+  };
+
   return (
     <>
       {colors.length > 0 && (
         <div className="my-2">
           <h3 className="text-md mb-2">Colors</h3>
-          <form className="flex flex-wrap gap-1">
+          <fieldset className="flex flex-wrap gap-1">
             {colors.map((color) => {
-              return <Radio name={color} key={color} />;
+              const colorSearchParams = new URLSearchParams(searchParams.toString());
+              colorSearchParams.set('color', color);
+              const colorUrl = createUrl(pathname, colorSearchParams);
+              const isChecked = searchParams.get('color') === color;
+              return (
+                <Radio
+                  name={color}
+                  key={color}
+                  onChange={() => updateParams(colorUrl)}
+                  checked={isChecked}
+                />
+              );
             })}
-          </form>
+          </fieldset>
         </div>
       )}
       {sizes.length > 0 && (
@@ -23,7 +47,18 @@ export function ProductForm(props: ProductFormProps) {
           <h3 className="text-md mb-2">Sizes</h3>
           <form className="flex flex-wrap gap-1">
             {sizes.map((size) => {
-              return <Radio name={size} key={size} />;
+              const sizeSearchParams = new URLSearchParams(searchParams.toString());
+              sizeSearchParams.set('size', size);
+              const sizeUrl = createUrl(pathname, sizeSearchParams);
+              const isChecked = searchParams.get('size') === size;
+              return (
+                <Radio
+                  name={size}
+                  key={size}
+                  onChange={() => updateParams(sizeUrl)}
+                  checked={isChecked}
+                />
+              );
             })}
           </form>
         </div>
@@ -34,13 +69,21 @@ export function ProductForm(props: ProductFormProps) {
 
 type RadioProps = {
   name: string;
+  onChange: () => void;
+  checked?: boolean;
 };
 function Radio(props: RadioProps) {
   const { name } = props;
   return (
     <label className="mr-1 flex justify-center rounded border px-2 py-1">
       {name}
-      <input className="ml-1" type="radio" name="sizes" />
+      <input
+        checked={props.checked}
+        onChange={props.onChange}
+        className="ml-1"
+        type="radio"
+        name="sizes"
+      />
     </label>
   );
 }
