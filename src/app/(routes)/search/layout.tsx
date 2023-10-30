ccccccf6c559318a2d492926cff8079676f33b49
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-10-29 20:59:31 -0400

    refactor: index files

--- src/app/(routes)/search/layout.tsx
+++ src/app/(routes)/search/layout.tsx
@@ -2,8 +2,8 @@ import { Suspense } from 'react';
 
 import { Container } from 'components/container/container';
 import { Footer } from 'components/layout/footer';
-import { Categories } from 'components/layout/search/categories/categories';
-import { Filter } from 'components/layout/search/filter/filter';
+import { Categories } from 'components/layout/search/categories';
+import { Filter } from 'components/layout/search/filter';
 import { sorting } from 'lib/constants';
 
 type SearchLayoutProps = {
