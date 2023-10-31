6fcd3fe99ee410dcbe2e9386e0400bd9cc19689b
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-10-31 19:33:11 -0400

    feat: Simplify cateogry page

--- src/app/(routes)/search/[category]/page.tsx
+++ src/app/(routes)/search/[category]/page.tsx
@@ -27,42 +27,27 @@ export default async function CategoryPage(props: CategoryPageProps) {
   });
 
   if (category.products.length === 0) {
-    return <p>No products found in this collection</p>;
+    return <p>No products found in this category.</p>;
   }
 
+  // TODO Use Next.js <Image /> component
   return (
     <Grid>
       {category.products.map((product) => {
         return (
           <GridItem key={product.id}>
             <Link href={`/product/${product.slug}`}>
-              <ProductImage image={product.images[0]} price={product.price} name={product.name} />
+              <div className="relative flex aspect-square h-full max-h-[550px] w-full flex-col items-center justify-center overflow-hidden">
+                <p className="text-sm">No images found for this product.</p>
+                <div className="absolute bottom-0 left-0">
+                  <h3>{product.name}</h3>
+                  <Price amount={String(product.price)} />
+                </div>
+              </div>{' '}
             </Link>
           </GridItem>
         );
       })}
     </Grid>
   );
-}
-
-type ProductImage = {
-  image?: string;
-  price: number;
-  name: string;
-};
-// TODO Alt text, image
-function ProductImage(props: ProductImage) {
-  if (props.image === undefined) {
-    return (
-      <div className="relative flex aspect-square h-full max-h-[550px] w-full flex-col items-center justify-center overflow-hidden">
-        <p className="text-sm">No images found for this product.</p>
-        <div className="absolute bottom-0 left-0">
-          <h3>{props.name}</h3>
-          <Price amount={String(props.price)} />
-        </div>
-      </div>
-    );
-  }
-
-  return <div className="aspect-square h-full max-h-[550px] w-full overflow-hidden">Todo</div>;
 }
