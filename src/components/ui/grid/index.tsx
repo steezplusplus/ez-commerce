1aea558c9accac0c9859ad5c8a1529dc2d7fa200
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-11-04 12:38:28 -0400

    feat: Grid acceps classname

--- src/components/ui/grid/index.tsx
+++ src/components/ui/grid/index.tsx
@@ -1,14 +1,22 @@
-export function Grid(props: { children: React.ReactNode }) {
+export function Grid(props: { children: React.ReactNode; className?: string }) {
   return (
-    <ul className="grid grid-flow-row grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+    <ul
+      className={`grid grid-flow-row grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 ${
+        props.className ? props.className : ''
+      }`}
+    >
       {props.children}
     </ul>
   );
 }
 
-export function GridItem(props: { children: React.ReactNode }) {
+export function GridItem(props: { children?: React.ReactNode; className?: string }) {
   return (
-    <li className="aspect-square rounded-sm border border-neutral-200 px-2 py-1 text-sm font-extralight hover:border-blue-500 dark:border-neutral-800">
+    <li
+      className={`aspect-square rounded-sm border border-neutral-200 px-2 py-1 text-sm font-extralight hover:border-blue-500 dark:border-neutral-800 ${
+        props.className ? props.className : ''
+      }`}
+    >
       {props.children}
     </li>
   );
