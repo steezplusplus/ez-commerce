9afbc901e826707c5c86403363404f0553fed939
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-11-04 13:18:47 -0400

    feat: Add error boundary, not found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="mx-auto my-4 flex max-w-xl flex-col">
+      <h2 className="text-xl font-bold">Oh no!</h2>
+      <p className="my-2">The requested resource could not be found.</p>
+    </div>
+  );
+}
