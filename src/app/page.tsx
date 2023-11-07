2f71ed36a5702ea462d26313168eccbdf1d3fba7
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-11-07 15:16:47 -0500

    feat: Loading layout styles consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,23 +11,27 @@ export default async function Home() {
 
   return (
     <>
-      <Container className="flex h-full items-center justify-center">
-        <Grid className="pb-4">
-          {products.map((product) => {
-            return (
-              <GridItem key={product.id}>
-                <Link className="block h-full w-full" href={`/product/${product.slug}`}>
-                  <div className="relative flex aspect-square h-full max-h-[550px] w-full flex-col items-center justify-center overflow-hidden">
-                    <p className="text-sm">No images found for this product.</p>
-                    <div className="absolute bottom-0 left-0">
-                      <h3>{product.name}</h3>
+      <Container className="flex flex-col gap-8 px-4 pb-4 md:flex-row">
+        <div className="order-first w-full flex-none md:max-w-[9rem]" />
+        <div className="order-last min-h-screen w-full md:order-none">
+          <Grid className="pb-4">
+            {products.map((product) => {
+              return (
+                <GridItem key={product.id}>
+                  <Link className="block h-full w-full" href={`/product/${product.slug}`}>
+                    <div className="relative flex aspect-square h-full max-h-[550px] w-full flex-col items-center justify-center overflow-hidden">
+                      <p className="text-sm">No images found for this product.</p>
+                      <div className="absolute bottom-0 left-0">
+                        <h3>{product.name}</h3>
+                      </div>
                     </div>
-                  </div>
-                </Link>
-              </GridItem>
-            );
-          })}
-        </Grid>
+                  </Link>
+                </GridItem>
+              );
+            })}
+          </Grid>
+        </div>
+        <div className="order-none flex-none md:order-last md:w-[9rem]" />
       </Container>
       <Suspense>
         <Footer />
