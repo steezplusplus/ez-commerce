3c2af987e4cf39f4d7e02355cb612ad38290751b
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-11-08 12:23:00 -0500

    feat: Create product grid

--- src/app/(routes)/search/page.tsx
+++ src/app/(routes)/search/page.tsx
@@ -1,7 +1,4 @@
-import Link from 'next/link';
-
-import { Grid, GridItem } from 'components/ui/grid';
-import { Price } from 'components/ui/price';
+import { ProductGrid } from 'components/ui/grid/product-grid';
 import { getSearchPage } from 'lib/api';
 import { sorting } from 'lib/constants';
 
@@ -19,10 +16,7 @@ type SearchPageProps = {
 export default async function SearchPage({ searchParams }: SearchPageProps) {
   const { q: searchValue, sort } = searchParams as { [key: string]: string };
   const sortKey = sorting.find((item) => item.slug === sort);
-
   const products = await getSearchPage({ searchValue: searchValue, order: sortKey?.order });
-  const numProducts = products.length;
-  const resultsText = numProducts > 1 ? 'results' : 'result';
 
   if (products.length === 0) {
     if (searchValue) {
@@ -35,31 +29,15 @@ export default async function SearchPage({ searchParams }: SearchPageProps) {
     return <p>There are no products in this store.</p>;
   }
 
-  // TODO Use Next.js <Image /> component
   return (
     <>
       {searchValue && (
         <p className="mb-3">
-          Showing {products.length} {resultsText} for <b>&quot;{searchValue}&quot;</b>.
+          Showing {products.length} {products.length > 1 ? 'results' : 'result'} for{' '}
+          <b>&quot;{searchValue}&quot;</b>.
         </p>
       )}
-      <Grid>
-        {products.map((product) => {
-          return (
-            <GridItem key={product.id}>
-              <Link className="block h-full w-full" href={`/product/${product.slug}`}>
-                <div className="relative flex aspect-square h-full max-h-[550px] w-full flex-col items-center justify-center overflow-hidden">
-                  <p className="text-sm">No images found for this product.</p>
-                  <div className="absolute bottom-0 left-0">
-                    <h3>{product.name}</h3>
-                    <Price amount={String(product.price)} />
-                  </div>
-                </div>
-              </Link>
-            </GridItem>
-          );
-        })}
-      </Grid>
+      <ProductGrid products={products} />
     </>
   );
 }
