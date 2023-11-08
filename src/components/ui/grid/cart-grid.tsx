68da19cc8bfdbf0dbba3efab77b69533119d74f8
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-11-08 14:43:12 -0500

    feat: Show images in cart

--- src/components/ui/grid/cart-grid.tsx
+++ src/components/ui/grid/cart-grid.tsx
@@ -7,6 +7,8 @@ import { RemoveFromCart } from 'components/checkout/remove-from-cart';
 import { Grid, GridItem } from 'components/ui/grid';
 import { Price } from 'components/ui/price';
 import { useCart } from 'hooks/use-cart';
+import { Palette, Ruler } from 'lucide-react';
+import Image from 'next/image';
 
 export function CartGrid() {
   const [isMounted, setIsMounted] = useState<boolean>(false);
@@ -30,19 +32,41 @@ export function CartGrid() {
       {cartItems.map((item) => {
         return (
           <GridItem key={item.product.id}>
-            <Link className="relative block h-full w-full" href={`/product/${item.product.slug}`}>
-              <div className="flex items-center justify-between">
-                <h3>{item.product.name}</h3>
-                <RemoveFromCart product={item.product} />
-              </div>
-              <div className=" flex aspect-square h-full max-h-[550px] w-full flex-col items-center justify-center overflow-hidden">
-                <div className="absolute bottom-0 left-0">
-                  {item.size && <p>Size: {item.size}</p>}
-                  {item.color && <p>Color: {item.color}</p>}
-                  <Price amount={String(item.product.price)} />
-                </div>
+            {/* Image */}
+            <Link href={`/product/${item.product.slug}`}>
+              <div className="relative flex aspect-square h-full max-h-[550px] w-full flex-col overflow-hidden">
+                {item.product.images.length === 0 ? (
+                  <p className="text-sm">No images found for this product.</p>
+                ) : (
+                  <Image
+                    className="aspect-square object-cover"
+                    src={item.product.images[0] || ''}
+                    fill
+                    sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, (max-width: 1024px) 33vw, 25vw"
+                    alt={`Image of ${item.product.name}`}
+                  />
+                )}
               </div>
             </Link>
+            {/* Actions */}
+            <div className="flex items-center justify-between">
+              <h3 className="mb-1 text-lg">{item.product.name}</h3>
+              <RemoveFromCart product={item.product} />
+            </div>
+            {/* Info */}
+            {item.size && (
+              <div className="flex items-center">
+                <Ruler size="18" className="mr-1" />
+                {item.size}
+              </div>
+            )}
+            {item.color && (
+              <div className="flex items-center">
+                <Palette size="18" className="mr-1" />
+                {item.color}
+              </div>
+            )}
+            <Price amount={String(item.product.price)} />
           </GridItem>
         );
       })}
