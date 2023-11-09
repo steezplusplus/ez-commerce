4a7b8cdf2c09f90348d1d6ce2fa06f5f60c163aa
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-11-09 15:03:21 -0500

    feat: Add Container to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { Suspense } from 'react';
 
 import { Footer } from 'components/layout/footer';
+import { Container } from 'components/ui/container';
 import { getHomePage } from 'lib/api';
 
 export default async function Home() {
@@ -10,30 +11,32 @@ export default async function Home() {
 
   return (
     <>
-      <div className="w-full overflow-x-auto">
-        <ul className="flex gap-4">
-          {products.map((product) => {
-            return (
-              <li
-                className="relative aspect-square h-[30vh] max-h-[275px] w-2/3 max-w-[475px] flex-none md:w-1/3"
-                key={product.id}
-              >
-                <Link href={`/product/${product.slug}`} className="relative h-full w-full">
-                  <div className="group relative flex h-full w-full items-center justify-center overflow-hidden rounded-lg border border-neutral-200 bg-white hover:border-blue-600 dark:border-neutral-800 dark:bg-black">
-                    <Image
-                      fill
-                      sizes="(min-width: 1024px) 25vw, (min-width: 768px) 33vw, 50vw"
-                      className="relative h-full w-full object-contain transition duration-300 ease-in-out group-hover:scale-105"
-                      src={product.images[0] as string}
-                      alt={`Image of ${product.name}`}
-                    />
-                  </div>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+      <Container>
+        <div className="w-full overflow-x-auto">
+          <ul className="flex gap-4">
+            {products.map((product) => {
+              return (
+                <li
+                  className="relative aspect-square h-[30vh] max-h-[275px] w-2/3 max-w-[475px] flex-none md:w-1/3"
+                  key={product.id}
+                >
+                  <Link href={`/product/${product.slug}`} className="relative h-full w-full">
+                    <div className="group relative flex h-full w-full items-center justify-center overflow-hidden rounded-lg border border-neutral-200 bg-white hover:border-blue-600 dark:border-neutral-800 dark:bg-black">
+                      <Image
+                        fill
+                        sizes="(min-width: 1024px) 25vw, (min-width: 768px) 33vw, 50vw"
+                        className="relative h-full w-full object-contain transition duration-300 ease-in-out group-hover:scale-105"
+                        src={product.images[0] as string}
+                        alt={`Image of ${product.name}`}
+                      />
+                    </div>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </Container>
 
       <Suspense>
         <Footer />
