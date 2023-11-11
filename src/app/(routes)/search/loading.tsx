c578895f24217f50481194f019225b41f5981fe5
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-11-11 13:22:01 -0500

    feat: Loading ui grid

--- src/app/(routes)/search/loading.tsx
+++ src/app/(routes)/search/loading.tsx
@@ -1,3 +1,15 @@
+import { Grid, GridItem } from 'components/ui/grid';
+
 export default function Loading() {
-  return <p>Grid TODO</p>;
+  return (
+    <Grid className="sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      {Array(12)
+        .fill(0)
+        .map((_, index) => {
+          return (
+            <GridItem key={index} className="animate-pulse bg-neutral-100 dark:bg-neutral-900" />
+          );
+        })}
+    </Grid>
+  );
 }
