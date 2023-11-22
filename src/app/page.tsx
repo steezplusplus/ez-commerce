4840cb5d47b3b710a636bb789a2dd0a4f694fe23
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-11-22 15:42:48 -0500

    fix: duplicate key

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default async function Home() {
         <ul className="grid grid-cols-1 gap-4 px-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {latestProducts.map((product) => {
             return (
-              <li key={product.id} className="relative aspect-square">
+              <li key={product.image} className="relative aspect-square">
                 <Link href={`/product/${product.slug}?color=${product.handle}`} className="h-full w-full">
                   <Image
                     fill
