517a84133a918504d1ab2dbbf9039b57148a9fe4
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-11-27 13:55:09 -0500

    chore: Remove unused componented

--- src/app/(routes)/search/loading.tsx
+++ src/app/(routes)/search/loading.tsx
@@ -1,13 +1,4 @@
-import { Grid, GridItem } from 'components/ui/grid';
-
+// TODO
 export default function Loading() {
-  return (
-    <Grid className="sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-      {Array(12)
-        .fill(0)
-        .map((_, index) => {
-          return <GridItem key={index} className="animate-pulse bg-neutral-100 dark:bg-neutral-900" />;
-        })}
-    </Grid>
-  );
+  return <div>Loading....</div>;
 }
