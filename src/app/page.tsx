5a4e5d1c65cdfa44679d3b135b3e5ee52dcf8220
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-11-27 13:22:55 -0500

    style: Fix Image warning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,15 +17,17 @@ export default async function Home() {
         <ul className="grid grid-cols-1 gap-4 px-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {latestProducts.map((product) => {
             return (
-              <li key={product.image} className="relative aspect-square">
+              <li key={product.image}>
                 <Link href={`/product/${product.slug}?color=${product.handle}`} className="h-full w-full">
-                  <Image
-                    fill
-                    alt={product.altText}
-                    src={product.image}
-                    className="aspect-square rounded-md object-cover"
-                    sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, (max-width: 1024px) 33vw, 25vw"
-                  />
+                  <div className="relative aspect-square rounded-md bg-gray-100">
+                    <Image
+                      fill
+                      alt={product.altText}
+                      src={product.image}
+                      className="aspect-square rounded-md object-cover"
+                      sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, (max-width: 1024px) 33vw, 25vw"
+                    />
+                  </div>
                 </Link>
               </li>
             );
@@ -35,15 +37,17 @@ export default async function Home() {
         <ul className="grid grid-cols-1 gap-4 px-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {featuredProducts.map((product) => {
             return (
-              <li key={product.id} className="relative aspect-square">
+              <li key={product.id}>
                 <Link href={`/product/${product.slug}?color=${product.handle}`} className="h-full w-full">
-                  <Image
-                    fill
-                    alt={product.altText}
-                    src={product.image}
-                    className="aspect-square rounded-md object-cover"
-                    sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, (max-width: 1024px) 33vw, 25vw"
-                  />
+                  <div className="relative aspect-square rounded-md bg-gray-100">
+                    <Image
+                      fill
+                      alt={product.altText}
+                      src={product.image}
+                      className="aspect-square rounded-md object-cover"
+                      sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, (max-width: 1024px) 33vw, 25vw"
+                    />
+                  </div>
                 </Link>
               </li>
             );
