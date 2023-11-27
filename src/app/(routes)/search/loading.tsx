f11d201dc0e29465a87453ed4187a0c5d6af29a6
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-11-27 13:59:29 -0500

    feat: Use container in loading

--- src/app/(routes)/search/loading.tsx
+++ src/app/(routes)/search/loading.tsx
@@ -1,4 +1,6 @@
-// TODO
+import { Container } from 'components/ui/container';
+
+// TODO Create a loading skeleton once the search page design is finalized
 export default function Loading() {
-  return <div>Loading....</div>;
+  <Container className="flex flex-col gap-8 px-4 pb-4 md:flex-row">Loading...</Container>;
 }
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,5 +2,5 @@ import { Container } from 'components/ui/container';
 
 // TODO Create a loading skeleton once the home page design is finalized
 export default function Loading() {
-  return <Container className="flex flex-col gap-8 px-4 pb-4 md:flex-row"></Container>;
+  return <Container className="flex flex-col gap-8 px-4 pb-4 md:flex-row">Loading...</Container>;
 }
