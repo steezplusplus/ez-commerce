29bf949c69afc25befadd6073bf9888c7e31b34d
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-12-04 15:50:51 -0500

    feat: Update image configuration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
-// TODO "images.domains" configuration is deprecated. Please use "images.remotePatterns" configuration instead.
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { domains: ['utfs.io'] },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
