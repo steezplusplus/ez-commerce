3a31d6edf4e0adc25131397c8b3ce5b9a380769f
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-12-04 20:30:38 -0500

    feat: Use some in featured products

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -88,8 +88,14 @@ export async function getCategoryPage(props: { name: string; order?: 'asc' | 'de
 }
 
 export async function getFeaturedProducts(props: { take: number }): Promise<Product[]> {
-  // TODO Not every product has a featured color
   const products = await prisma.product.findMany({
+    where: {
+      colors: {
+        some: {
+          isFeatured: true,
+        },
+      },
+    },
     include: {
       colors: {
         where: {
@@ -100,17 +106,18 @@ export async function getFeaturedProducts(props: { take: number }): Promise<Prod
     take: props.take,
   });
 
-  // TODO assertions that every product has an image
-  return products.map((product) => {
-    return {
-      id: product.id,
-      name: product.name,
-      slug: product.slug,
-      price: product.price,
-      handle: product.colors[0]?.value as string,
-      image: product.colors[0]?.image as string,
-      altText: product.colors[0]?.altText as string,
-    };
+  return products.flatMap((product) => {
+    return product.colors.map((color) => {
+      return {
+        id: color.id,
+        handle: color.value,
+        image: color.image,
+        altText: color.altText,
+        name: product.name,
+        slug: product.slug,
+        price: product.price,
+      };
+    });
   });
 }
 
