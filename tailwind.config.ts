dc0e6b809ea3b4bb113bff474f4770137799d8cb
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-12-07 13:11:06 -0500

    chore: Remove unused tailwind theming

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,24 +7,6 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/providers/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      animation: {
-        marquee: 'marquee 60s linear infinite',
-        marquee2: 'marquee2 60s linear infinite',
-      },
-      keyframes: {
-        marquee: {
-          '0%': { transform: 'translateX(0%)' },
-          '100%': { transform: 'translateX(-100%)' },
-        },
-        marquee2: {
-          '0%': { transform: 'translateX(100%)' },
-          '100%': { transform: 'translateX(0%)' },
-        },
-      },
-    },
-  },
   plugins: [],
 };
 export default config;
