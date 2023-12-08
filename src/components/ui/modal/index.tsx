fb5930cb1574820b7ebbd7e80a2bfb84d9e5f20f
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-12-08 13:54:19 -0500

    feat: Create Modal component

--- src/components/ui/modal/index.tsx
+++ src/components/ui/modal/index.tsx
@@ -1,35 +1,47 @@
+'use client';
+
+import { Dialog, Transition } from '@headlessui/react';
 import { X } from 'lucide-react';
+import { Fragment } from 'react';
 
 type ModalProps = {
-  modalDialogRef: React.RefObject<HTMLDialogElement>;
-  labelId: string;
-  descriptionId: string;
-  title: string;
+  open: boolean;
+  onClose: () => void;
   children: React.ReactNode;
 };
 
 export function Modal(props: ModalProps) {
-  const closeModalDialog = () => {
-    if (props.modalDialogRef.current) {
-      props.modalDialogRef.current.close();
-    }
-  };
+  const { open, onClose, children } = props;
 
   return (
-    <dialog
-      ref={props.modalDialogRef}
-      aria-labelledby={props.labelId}
-      aria-describedby={props.descriptionId}
-      autoFocus
-      className="w-1/2 rounded-lg border border-neutral-200 px-4 py-2 dark:border-neutral-800"
-    >
-      <div className="mb-4 flex items-center justify-between">
-        <h3 className="text-lg">{props.title}</h3>
-        <button onClick={closeModalDialog} className="rounded-md border border-neutral-200 p-1 dark:border-neutral-800">
-          <X size="16" />
-        </button>
-      </div>
-      {props.children}
-    </dialog>
+    <Transition show={open} appear as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={onClose}>
+        <div className="fixed inset-0 bg-black bg-opacity-50" />
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="w-full max-w-3xl overflow-hidden rounded-lg text-left align-middle">
+                <div className="relative flex w-full items-center overflow-hidden bg-white px-4 pb-8 pt-14 shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
+                  <div className="absolute right-4 top-4">
+                    <button onClick={onClose}>
+                      <X />
+                    </button>
+                  </div>
+                  {children}
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
   );
 }
