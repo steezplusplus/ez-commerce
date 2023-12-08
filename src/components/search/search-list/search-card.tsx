34eac50f2bcf4a7316e4c57988214e6e7bf1a94b
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-12-08 14:41:24 -0500

    feat: Add btns to card

--- src/components/search/search-list/search-card.tsx
+++ src/components/search/search-list/search-card.tsx
@@ -1,10 +1,8 @@
-import { Expand } from 'lucide-react';
-import Image from 'next/image';
 import Link from 'next/link';
 
 import { Price } from 'components/ui/price';
-import { usePreviewModal } from 'hooks/use-preview-modal';
 import { Product } from 'lib/types';
+import { SearchCardImage } from './search-card-image';
 
 export function SearchCard({ product }: { product: Product }) {
   const { id, name, slug, handle, image, altText, price } = product;
@@ -18,40 +16,6 @@ export function SearchCard({ product }: { product: Product }) {
         <SearchCardInfo name={name} price={price} />
       </Link>
     </li>
-  );
-}
-
-function SearchCardImage({ image, altText }: { image: string; altText: string }) {
-  'use client';
-  const previewModal = usePreviewModal();
-
-  const onPreview = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    previewModal.onOpen([]);
-  };
-
-  return (
-    <div
-      className="
-        relative aspect-square rounded-md 
-        bg-gray-100 dark:bg-gray-700
-      "
-    >
-      <Image
-        fill
-        alt={altText}
-        src={image}
-        className="aspect-square rounded-md object-cover"
-        sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, (max-width: 1024px) 33vw, 25vw"
-      />
-      <div className="absolute bottom-5 w-full px-6 opacity-0 transition group-hover:opacity-100 ">
-        <div className="flex justify-center gap-x-6">
-          <button onClick={onPreview}>
-            <Expand size={20} className="text-gray-600" />
-          </button>
-        </div>
-      </div>
-    </div>
   );
 }
 
