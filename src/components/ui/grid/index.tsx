9ab696e2336a6bc618bd3c9ec62d840ddcc32caa
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-12-08 16:17:25 -0500

    feat: Create reusable grid

---
+++ src/components/ui/grid/index.tsx
@@ -0,0 +1,19 @@
+function Grid(props: React.ComponentProps<'ul'>) {
+  return (
+    <ul {...props} className={`grid grid-flow-row gap-4 ${props.className}`}>
+      {props.children}
+    </ul>
+  );
+}
+
+function GridItem(props: React.ComponentProps<'li'>) {
+  return (
+    <li {...props} className={`aspect-square transition-opacity ${props.className}`}>
+      {props.children}
+    </li>
+  );
+}
+
+Grid.Item = GridItem;
+
+export default Grid;
