fc2642f0a6a2ee6b2b0f13a8ca476d5c0090e4a0
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-12-09 00:05:24 -0500

    feat: Add 'All' category

--- src/components/layout/footer/footer-menu.tsx
+++ src/components/layout/footer/footer-menu.tsx
@@ -2,12 +2,15 @@ import { Category } from '@prisma/client';
 import Link from 'next/link';
 
 export function FooterMenu({ categories }: { categories: Category[] }) {
-  if (!categories.length) return null;
-
   return (
     <nav>
       <h3 className="mb-1">Categories</h3>
       <ul>
+        <li>
+          <Link href="/search" className="text-sm font-light text-blue-500 hover:text-blue-700 hover:underline">
+            All
+          </Link>
+        </li>
         {categories.map((category: Category) => {
           return <FooterMenuItem key={category.id} category={category} />;
         })}
