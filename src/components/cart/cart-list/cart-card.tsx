467b4d6ed970aff69677c1ebc9ef7307d4f91cf6
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-12-11 18:06:14 -0500

    feat: Fix styling, add todo

--- src/components/cart/cart-list/cart-card.tsx
+++ src/components/cart/cart-list/cart-card.tsx
@@ -1,17 +1,17 @@
 import Link from 'next/link';
 
-import { GridItem } from 'components/ui/grid';
 import { CartItem } from 'hooks/use-cart';
 import { CartImage } from './cart-image';
 import { CartInfo } from './cart-info';
 
+// TODO Add slug to CartItem so I can link back to the product
 export function CartCard({ cartItem }: { cartItem: CartItem }) {
   return (
-    <GridItem className="flex aspect-auto">
-      <Link href="/">
+    <li className="flex aspect-auto">
+      <Link href={`/`}>
         <CartImage src={cartItem.image} alt={cartItem.altText} />
       </Link>
       <CartInfo cartItem={cartItem} />
-    </GridItem>
+    </li>
   );
 }
