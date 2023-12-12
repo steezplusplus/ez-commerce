ec1c11d81f6fc5ec971058203858e56cbb16f9d4
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-12-12 16:54:12 -0500

    style: Product info spacing and typography

--- src/components/cart/cart-list/cart-info.tsx
+++ src/components/cart/cart-list/cart-info.tsx
@@ -4,16 +4,16 @@ import { RemoveFromCart } from '../remove-from-cart';
 
 export function CartInfo({ cartItem }: { cartItem: CartItem }) {
   return (
-    <div className="flex flex-1 flex-col justify-between">
+    <div className="flex w-full flex-col">
       <div className="flex justify-between">
         <h3 className="text-lg font-semibold">{cartItem.name}</h3>
         <RemoveFromCart inventoryId={cartItem.id} />
       </div>
-      <h4 className="text-sm">{cartItem.color}</h4>
-      <h4 className="text-sm">{cartItem.size}</h4>
-      <h4 className="text-sm">
+      <h4 className="text-md mb-1">
         <Price amount={String(cartItem.price)} />
       </h4>
+      <h4 className="mb-1 text-sm">{cartItem.color}</h4>
+      <h4 className="text-sm">{cartItem.size}</h4>
     </div>
   );
 }
