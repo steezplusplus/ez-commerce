25069e6dd54152a9120aec51c115542c05389968
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-12-18 17:34:22 -0500

    feat: Create ad-hoc product modal component

--- src/components/layout/search/product-list/product-modal.tsx
+++ src/components/layout/search/product-list/product-modal.tsx
@@ -6,6 +6,7 @@ import { Gallery } from 'components/ui/gallery';
 import { Modal } from 'components/ui/modal';
 import { Price } from 'components/ui/price';
 import { useProductModal } from 'hooks/use-product-modal';
+import { ProductWithColor } from 'lib/api';
 
 // TODO place of link. Styling of price. Make second col its own component.
 export function ProductModal() {
@@ -22,33 +23,41 @@ export function ProductModal() {
         <div className="sm:col-span-4 lg:col-span-5">
           <Gallery colors={productData.colors} />
         </div>
-        <div className="space-y-4 sm:col-span-8 lg:col-span-7">
-          <h4>{productData.description}</h4>
-          <h4>
-            <Price amount={String(productData.price)} />
-          </h4>
-          <Link
-            className="
-              flex
-              items-center
-              justify-center
-              rounded-md
-              border
-              border-neutral-200
-              bg-white
-              p-2
-              transition
-              hover:opacity-75
-              dark:border-neutral-800
-              dark:bg-transparent
-            "
-            href={`/product/${productData.slug}`}
-            onClick={() => productModal.onClose()}
-          >
-            Go to product page
-          </Link>
+        <div className="h-full sm:col-span-8 lg:col-span-7">
+          <ProductModalInfo product={productData} onClose={productModal.onClose} />
         </div>
       </div>
     </Modal>
+  );
+}
+
+function ProductModalInfo({ product, onClose }: { product: ProductWithColor; onClose: () => void }) {
+  return (
+    <div className="flex h-full flex-col gap-y-4">
+      <h4>{product.description}</h4>
+      <h4>
+        <Price amount={String(product.price)} />
+      </h4>
+      <Link
+        className=" 
+          flex
+          items-center
+          justify-center
+          rounded-md
+          border
+          border-neutral-200
+          bg-white
+          p-2
+          transition
+          hover:opacity-75
+          dark:border-neutral-800
+          dark:bg-transparent
+        "
+        href={`/product/${product.slug}`}
+        onClick={() => onClose()}
+      >
+        Go to product page
+      </Link>
+    </div>
   );
 }
