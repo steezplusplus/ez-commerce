bdb048b97a674b27720b0aa4595aa2887ea694e8
Jesse Breuer <penelloj2@gmail.com>
2023-12-18 19:16:42 -0500

    Dev/headless-ui (#16)
    * feat: Init new Gallery component
    
    * feat: Recreate gallery component
    
    * feat: Create ad-hoc product modal component
    
    * feat: Rework modal and priduct modal
    
    * style: Search modal spacing
    
    * chore: Add todos

--- src/components/layout/search/product-list/product-modal.tsx
+++ src/components/layout/search/product-list/product-modal.tsx
@@ -1,13 +1,12 @@
 'use client';
 
-import Image from 'next/image';
 import Link from 'next/link';
 
-import { Tab } from '@headlessui/react';
-import { Color } from '@prisma/client';
+import { Gallery } from 'components/ui/gallery';
 import { Modal } from 'components/ui/modal';
 import { Price } from 'components/ui/price';
 import { useProductModal } from 'hooks/use-product-modal';
+import { ProductWithColor } from 'lib/api';
 
 export function ProductModal() {
   const productModal = useProductModal();
@@ -21,104 +20,53 @@ export function ProductModal() {
     <Modal open={productModal.isOpen} onClose={productModal.onClose} title={productData.name}>
       <div className="grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
         <div className="sm:col-span-4 lg:col-span-5">
-          <Gallery name={productData.name} images={productData.colors} />
+          <Gallery colors={productData.colors} />
         </div>
-
-        <div className="space-y-4 sm:col-span-8 lg:col-span-7">
-          <h4>{productData.description}</h4>
-          <h4>
-            <Price amount={String(productData.price)} />
-          </h4>
-          <Link
-            className="
-              flex
-              items-center
-              justify-center
-              rounded-md
-              border
-              border-neutral-200
-              bg-white
-              p-2
-              transition
-              hover:opacity-75
-              dark:border-neutral-800
-              dark:bg-transparent
-            "
-            href={`/product/${productData.slug}`}
-            onClick={() => productModal.onClose()}
-          >
-            Go to product page
-          </Link>
+        <div className="h-full sm:col-span-8 lg:col-span-7">
+          <ProductModalInfo product={productData} onClose={productModal.onClose} />
         </div>
       </div>
+      <ProductModalLink slug={productData.slug} onClose={productModal.onClose} />
     </Modal>
   );
 }
 
-type GalleryProps = {
-  images: Color[];
-  name: string;
-};
-
-export function Gallery(props: GalleryProps) {
-  const { images, name } = props;
+function ProductModalInfo({ product, onClose }: { product: ProductWithColor; onClose: () => void }) {
   return (
-    <Tab.Group as="div" className="flex flex-col-reverse">
-      <div className="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
-        <Tab.List className="grid grid-cols-4 gap-6">
-          {images.map((image) => (
-            <GalleryTab key={image.id} image={image} name={name} />
-          ))}
-        </Tab.List>
-      </div>
-      <Tab.Panels className="aspect-square w-full">
-        {images.map((image) => (
-          <Tab.Panel key={image.id}>
-            <div className="relative aspect-square h-full w-full overflow-hidden sm:rounded-lg">
-              <Image
-                fill
-                src={image.image}
-                alt={image.altText}
-                className="object-cover object-center"
-                sizes="(max-width: 1024px) 100vw, 50vw"
-                priority={true}
-              />
-            </div>
-          </Tab.Panel>
-        ))}
-      </Tab.Panels>
-    </Tab.Group>
+    <div className="flex h-full flex-col gap-y-4">
+      <h4>{product.description}</h4>
+      <h4>
+        <Price amount={String(product.price)} />
+      </h4>
+    </div>
   );
 }
 
-type GalleryTabProps = {
-  image: Color;
-  name: string;
-};
-
-function GalleryTab(props: GalleryTabProps) {
-  const { image, name } = props;
+function ProductModalLink({ slug, onClose }: { slug: string; onClose: () => void }) {
   return (
-    <Tab className="relative flex aspect-square cursor-pointer items-center justify-center rounded-md bg-gray-100 dark:bg-gray-700">
-      {({ selected }) => (
-        <div>
-          <span className="absolute inset-0 aspect-square h-full w-full overflow-hidden rounded-md">
-            <Image
-              fill
-              src={image.image}
-              alt={image.altText}
-              className="object-cover object-center"
-              sizes="(max-width: 1024px) 100vw, 20vw"
-            />
-          </span>
-          <span
-            className={`
-              absolute inset-0 rounded-md ring-2 ring-offset-2
-              ${selected ? 'ring-black' : 'ring-transparent'}
-            `}
-          />
-        </div>
-      )}
-    </Tab>
+    <Link
+      className=" 
+        mt-4
+        flex
+        w-full
+        justify-center
+        rounded-md
+        border
+        bg-neutral-900
+        px-2
+        py-1
+        text-sm
+        tracking-widest
+        text-white
+        transition
+        hover:opacity-75
+        dark:bg-neutral-50
+        dark:text-black
+      "
+      href={`/product/${slug}`}
+      onClick={() => onClose()}
+    >
+      Go to product page
+    </Link>
   );
 }
