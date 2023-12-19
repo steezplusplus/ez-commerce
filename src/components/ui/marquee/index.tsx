fb1d373fc170af4f41dabc3e5c9acc511808c0ba
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-12-19 15:44:45 -0500

    feat: Show price and name, add link

--- src/components/ui/marquee/index.tsx
+++ src/components/ui/marquee/index.tsx
@@ -1,6 +1,9 @@
-import { Color } from '@prisma/client';
-import { ProductWithColor } from 'lib/api';
 import Image from 'next/image';
+import Link from 'next/link';
+
+import { Color } from '@prisma/client';
+import { Price } from 'components/ui/price';
+import { ProductWithColor } from 'lib/api';
 
 export function Marquee({ products }: { products: ProductWithColor[] }) {
   return (
@@ -37,8 +40,6 @@ export function Marquee({ products }: { products: ProductWithColor[] }) {
   );
 }
 
-// TODO Show product color.name, product.name, product.price on hover.
-// TODO On click route to product page with color in query params.
 function MarqueeFrame({ colors, price, name, slug }: { colors: Color[]; price: number; name: string; slug: string }) {
   const manyColors = [...colors, ...colors, ...colors];
   return (
@@ -46,7 +47,19 @@ function MarqueeFrame({ colors, price, name, slug }: { colors: Color[]; price: n
       {manyColors.map((color) => {
         return (
           <li key={color.id} className="relative mx-2 h-24 w-24 overflow-hidden rounded-md sm:h-48 sm:w-48">
-            <Image src={color.image} alt={color.altText} fill sizes="33vw" className="object-cover object-center" />
+            <Link href={`/product/${slug}?color=${color.value}`} className="relative h-full w-full">
+              <div className="group flex h-full w-full items-center justify-center overflow-hidden rounded-lg border bg-white hover:border-blue-600 dark:bg-black">
+                <Image src={color.image} alt={color.altText} fill sizes="33vw" className="object-cover object-center" />
+                <div className="absolute bottom-0 left-0 flex w-full px-4 pb-4">
+                  <div className="flex items-center rounded-full border bg-white/70 p-1 text-xs font-semibold text-black backdrop-blur-md dark:border-neutral-800 dark:bg-black/70 dark:text-white">
+                    <h3 className="mr-4 line-clamp-2 flex-grow pl-2 leading-none tracking-tight">{name}</h3>
+                    <span className="flex-none rounded-full bg-blue-600 p-2 text-white">
+                      <Price amount={String(price)} />
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </Link>
           </li>
         );
       })}
