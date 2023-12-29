d790f2493230de3a1cb38059f5c3f70acbc754ab
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-12-29 18:46:54 -0500

    refactor: Update test description for homepage

--- __tests__/homepage.test.tsx
+++ __tests__/homepage.test.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 import Page from '../app/(routes)/page';
 
-describe('Homepage', () => {
+describe('Home page', () => {
   it('renders with no errors', async () => {
     await act(async () => {
       render(await Page());
