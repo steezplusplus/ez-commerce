b1ab798c11cada5955339c5bc9f3e3a0372dd259
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-12-30 15:27:10 -0500

    feat: Find a pattern that works

--- __tests__/app/home-page.test.tsx
+++ __tests__/app/home-page.test.tsx
@@ -1,13 +1,10 @@
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
-import { act } from 'react-dom/test-utils';
 
 import Page from '@/app/(routes)/page';
 
-describe('Home page', () => {
+describe('Page', () => {
   it('renders with no errors', async () => {
-    await act(async () => {
-      render(await Page());
-    });
+    render(await Page());
   });
 });
