b41e76d9906129f825ca0d7a6cad436d92f9a5d5
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-12-29 21:11:33 -0500

    refactor: update import

--- __tests__/homepage.test.tsx
+++ __tests__/homepage.test.tsx
@@ -1,7 +1,8 @@
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
-import Page from '../app/(routes)/page';
+
+import Page from '@/app/(routes)/page';
 
 describe('Home page', () => {
   it('renders with no errors', async () => {
