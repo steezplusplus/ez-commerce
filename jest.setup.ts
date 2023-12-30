e0f430c83306a343868ffde507fa92e5ab9e44b1
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-12-30 18:16:54 -0500

    feat: Mock nextjs navigation hooks

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,18 @@
 import '@testing-library/jest-dom';
+
+jest.mock('next/navigation', () => {
+  return {
+    __esModule: true,
+    usePathname: () => ({
+      pathname: '',
+    }),
+    useRouter: () => ({
+      push: jest.fn(),
+      replace: jest.fn(),
+      prefetch: jest.fn(),
+    }),
+    useSearchParams: () => ({
+      get: () => {},
+    }),
+  };
+});
