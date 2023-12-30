b5b86dc7233d3dba16e0466e97f145043675de9d
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-12-30 15:39:32 -0500

    feat: Search page renders w/ no errors

--- __tests__/app/search-page.test.tsx
+++ __tests__/app/search-page.test.tsx
@@ -1,5 +1,10 @@
-describe('Search page', () => {
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+
+import Page from '@/app/(routes)/search/page';
+
+describe('Search Page', () => {
   it('renders with no errors', async () => {
-    expect(true);
+    render(await Page({ searchParams: { q: '', sort: '' } }));
   });
 });
