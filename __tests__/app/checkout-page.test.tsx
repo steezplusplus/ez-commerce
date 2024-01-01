b06fc651427f79529bc50c4d2a34f53a52de67c2
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2024-01-01 17:23:22 -0500

    test: Checkout page test

--- __tests__/app/checkout-page.test.tsx
+++ __tests__/app/checkout-page.test.tsx
@@ -1,10 +1,21 @@
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import Page from '@/app/(routes)/checkout/page';
 
+// const productPrice = screen.getByRole('heading', { level: 4 });
 describe('Checkout Page', () => {
-  it('renders with no errors', () => {
+  it('Renders expected elements', () => {
     render(<Page />);
+    const checkoutHeading = screen.getByRole('heading', { level: 2, name: 'Checkout' });
+    const emptyCartHeading = screen.getByRole('heading', { level: 3, name: 'Your cart currently has no products.' });
+    const summaryHeading = screen.getByRole('heading', { level: 2, name: 'Order summary' });
+    const totalHeading = screen.getByRole('heading', { level: 3, name: 'Order total' });
+    const checkoutBtn = screen.getByRole('button', { name: 'Checkout' });
+    expect(checkoutHeading).toBeInTheDocument();
+    expect(emptyCartHeading).toBeInTheDocument();
+    expect(summaryHeading).toBeInTheDocument();
+    expect(totalHeading).toBeInTheDocument();
+    expect(checkoutBtn).toBeInTheDocument();
   });
 });
