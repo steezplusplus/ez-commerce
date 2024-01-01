a61dce446b2ecd668d898677c53069656b9fb8b3
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2023-12-31 21:07:10 -0500

    feat: Basic homepage tests

--- __tests__/app/home-page.test.tsx
+++ __tests__/app/home-page.test.tsx
@@ -1,10 +1,14 @@
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import Page from '@/app/(routes)/page';
 
 describe('Home Page', () => {
-  it('renders with no errors', async () => {
+  it('renders expected elements', async () => {
     render(await Page());
+    const Marqueeh2 = screen.getByText('Build your dream closet');
+    const Marqueeh3 = screen.getByText('& inspire your next style');
+    expect(Marqueeh2).toBeInTheDocument();
+    expect(Marqueeh3).toBeInTheDocument();
   });
 });
