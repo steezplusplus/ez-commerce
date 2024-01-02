bed64a50953b63dc66e5e7ad793740cddd53c4f0
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2024-01-02 13:22:38 -0500

    test: use accessible matchers

--- __tests__/app/home-page.test.tsx
+++ __tests__/app/home-page.test.tsx
@@ -6,9 +6,9 @@ import Page from '@/app/(routes)/page';
 describe('Home Page', () => {
   it('renders expected elements', async () => {
     render(await Page());
-    const Marqueeh2 = screen.getByText('Build your dream closet');
-    const Marqueeh3 = screen.getByText('& inspire your next style');
-    expect(Marqueeh2).toBeInTheDocument();
-    expect(Marqueeh3).toBeInTheDocument();
+    const marqueeh2 = screen.getByRole('heading', { level: 2, name: 'Build your dream closet' });
+    const marqueeh3 = screen.getByRole('heading', { level: 3, name: '& inspire your next style' });
+    expect(marqueeh2).toBeInTheDocument();
+    expect(marqueeh3).toBeInTheDocument();
   });
 });
