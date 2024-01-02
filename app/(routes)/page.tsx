090fc4b18563f3431b6a01632d4e6d20b5cd2aae
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2024-01-02 15:54:37 -0500

    feat: Setup homepage layout

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,47 +1,46 @@
 import { Carousel } from '@/components/ui/carousel';
 import { Container } from '@/components/ui/container';
 import { FastMarquee, SlowMarquee } from '@/components/ui/marquee';
-import { ProductWithColor, getFeaturedProducts } from '@/lib/api';
+import { getFeaturedProducts } from '@/lib/api';
 
 export default async function HomePage() {
   const featuredProducts = await getFeaturedProducts({ take: 9 });
   return (
     <Container className="px-4">
-      <Marquees featuredProducts={featuredProducts} />
+      <section>
+        <div className="mb-2 w-full p-1">
+          <h2 id="carousel-latest-id" className="my-2 text-xl font-medium">
+            Shop Latest Arrivals
+          </h2>
+          <Carousel ariaLabelledBy="carousel-latest-id" products={featuredProducts} />
+        </div>
+      </section>
+
+      <section className="flex flex-col gap-x-8 md:flex-row">
+        <div className="my-2 w-full p-1">
+          <h2 id="carousel-category1-id" className="my-2 text-xl font-medium">
+            Shop Headware
+          </h2>
+          <Carousel ariaLabelledBy="carousel-category1-id" products={featuredProducts} />
+        </div>
+        <div className="my-2 w-full p-1">
+          <h2 id="carousel-category2-id" className="my-2 text-xl font-medium">
+            Shop Tops
+          </h2>
+          <Carousel ariaLabelledBy="carousel-category2-id" products={featuredProducts} />
+        </div>
+      </section>
+
+      <section>
+        <div className="flex flex-col items-center justify-center">
+          <h2 className="mb-1 text-xl font-medium">Build your dream closet</h2>
+          <h3 className="mb-3 font-thin">& inspire your next style</h3>
+        </div>
+        <div className="space-y-2">
+          <SlowMarquee products={featuredProducts} />
+          <FastMarquee products={featuredProducts} />
+        </div>
+      </section>
     </Container>
-  );
-}
-
-function Marquees({ featuredProducts }: { featuredProducts: ProductWithColor[] }) {
-  return (
-    <section>
-      <div className="flex flex-col items-center justify-center">
-        <h2 className="mb-1 text-xl font-medium">Build your dream closet</h2>
-        <h3 className="mb-3 font-thin">& inspire your next style</h3>
-      </div>
-      <div className="space-y-2">
-        <SlowMarquee products={featuredProducts} />
-        <FastMarquee products={featuredProducts} />
-      </div>
-    </section>
-  );
-}
-
-function Carousels({ featuredProducts }: { featuredProducts: ProductWithColor[] }) {
-  return (
-    <section className="flex flex-col gap-x-8 md:flex-row">
-      <div className="my-2 w-full p-1">
-        <h2 id="carousel-category1-id" className="my-2 text-xl font-medium">
-          Category 1
-        </h2>
-        <Carousel ariaLabelledBy="carousel-category1-id" products={featuredProducts} />
-      </div>
-      <div className="my-2 w-full p-1">
-        <h2 id="carousel-category2-id" className="my-2 text-xl font-medium">
-          Category2
-        </h2>
-        <Carousel ariaLabelledBy="carousel-category2-id" products={featuredProducts} />
-      </div>
-    </section>
   );
 }
