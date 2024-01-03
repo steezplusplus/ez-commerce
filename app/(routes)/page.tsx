8dd5a93a70522b69f8e6aef6de38738ad83e645c
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2024-01-02 19:07:06 -0500

    feat: shorcut homepage

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,4 +1,4 @@
-import { Carousel } from '@/components/ui/carousel';
+import { LatestArrivalList } from '@/components/layout/home/latest-arrivals-list';
 import { Container } from '@/components/ui/container';
 import { FastMarquee, SlowMarquee } from '@/components/ui/marquee';
 import { getFeaturedProducts, getLatestArrivals } from '@/lib/api';
@@ -9,46 +9,12 @@ export default async function HomePage() {
 
   return (
     <Container className="px-4">
-      <section>
-        <div className="mb-2 w-full p-1">
-          <h2 id="carousel-latest-id" className="my-2 text-xl font-medium">
-            Shop Latest Arrivals
-          </h2>
-          <Carousel ariaLabelledBy="carousel-latest-id" products={latestArrivals} />
-        </div>
+      <section className="py-2">
+        <h2 className="mb-2 text-xl font-medium">Shop Latest Arrivals</h2>
+        <LatestArrivalList products={latestArrivals} />
       </section>
 
-      <section className="flex flex-col gap-x-8 md:flex-row">
-        <div className="my-2 w-full p-1">
-          <h2 id="carousel-headware-id" className="my-2 text-xl font-medium">
-            Shop Headware
-          </h2>
-          <Carousel ariaLabelledBy="carousel-headware-id" products={featuredProducts} />
-        </div>
-        <div className="my-2 w-full p-1">
-          <h2 id="carousel-tops-id" className="my-2 text-xl font-medium">
-            Shop Tops
-          </h2>
-          <Carousel ariaLabelledBy="carousel-tops-id" products={featuredProducts} />
-        </div>
-      </section>
-
-      <section className="flex flex-col gap-x-8 md:flex-row">
-        <div className="my-2 w-full p-1">
-          <h2 id="carousel-socks-id" className="my-2 text-xl font-medium">
-            Shop Socks
-          </h2>
-          <Carousel ariaLabelledBy="carousel-socks-id" products={featuredProducts} />
-        </div>
-        <div className="my-2 w-full p-1">
-          <h2 id="carousel-drinkware-id" className="my-2 text-xl font-medium">
-            Shop Drinkware
-          </h2>
-          <Carousel ariaLabelledBy="carousel-drinkware-id" products={featuredProducts} />
-        </div>
-      </section>
-
-      <section>
+      <section className="py-2">
         <div className="flex flex-col items-center justify-center">
           <h2 className="mb-1 text-xl font-medium">Build your dream closet</h2>
           <h3 className="mb-3 font-thin">& inspire your next style</h3>
